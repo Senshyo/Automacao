ca44656c4b7f616c730d7b2f00d04775fcd80847
Marcelo (Senshyo) <ct.marcelo@hotmail.com>
2023-10-01 13:46:37 -0300

    Atualização projeto

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Automacao
-Projeto simples de automação para estudo
+# Projeto de Estudo - Automação com Selenium e Pandas
+
+Este é um projeto de estudo que demonstra a automação de preenchimento de formulários da web usando as bibliotecas Selenium e Pandas em Python.
+
+## Descrição
+
+O objetivo deste projeto é automatizar o preenchimento de formulários em um site da web com base em dados de um arquivo CSV. O script lê os dados do arquivo CSV usando o Pandas e, em seguida, usa o Selenium para interagir com o site e preencher os campos do formulário com os dados do CSV.
+
+## Pré-requisitos
+
+Antes de executar este projeto, certifique-se de ter instalado as seguintes dependências:
+
+- [Python](https://www.python.org/) (versão 3.6 ou posterior)
+- [Selenium](instalado via pip)
+- [Pandas](instalado via pip)
+
+Você pode instalar as dependências usando o `pip` com o seguinte comando:
+
+bash
+pip install -r requirements.txt
+
+## Uso
+Execute o script com o seguinte comando:
+
+bash
+python script.py
+
+## Contribuição
+Este é um projeto de estudo pessoal, mas contribuições e sugestões são bem-vindas.
+
+## Licença
+Este projeto é de código aberto e está disponível sob a Licença MIT.
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os 
+import time
+
+def main():
+    #Carrega a tabela
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(script_dir,"data\\produtos.csv")
+    tabela = pd.read_csv(path)
+
+    #instancia o driver
+    driver = webdriver.Chrome()
+
+    #loga no site
+    driver.get("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+    driver.find_element(By.ID, "email").send_keys("email@gmail.com")
+    driver.find_element(By.ID, "password").send_keys("PASSWORD")
+    driver.find_element(By.ID, "pgtpy-botao").click()
+    time.sleep(1)
+    
+
+    #preenche os produtos de acordo com a tabela encontrada
+    for linha in tabela.index:
+        driver.find_element(By.ID,"codigo").send_keys(str(tabela.loc[linha,"codigo"]))
+        driver.find_element(By.ID,"marca").send_keys(str(tabela.loc[linha,"marca"]))
+        driver.find_element(By.ID,"tipo").send_keys(str(tabela.loc[linha,"tipo"]))
+        driver.find_element(By.ID,"categoria").send_keys(str(tabela.loc[linha,"categoria"]))
+        driver.find_element(By.ID,"preco_unitario").send_keys(str(tabela.loc[linha,"preco_unitario"]))
+        driver.find_element(By.ID,"custo").send_keys(str(tabela.loc[linha,"custo"]))
+        obs = tabela.loc[linha,"obs"]
+        if(not pd.isna(obs)):
+            driver.find_element(By.ID,"obs").send_keys(str(obs))
+        driver.find_element(By.ID,"pgtpy-botao").click()
+    
+
+if __name__ == "__main__":
+    main()
